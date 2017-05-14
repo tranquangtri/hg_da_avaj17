8b0154e413919316705be232e7c4a5c21e1b21cd
tranquangtri <tranquangtri96@live.com>
2017-05-14 14:21:14 +0700

    t

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -3,11 +3,7 @@
  */
 import java.util.*;
  public class GameServer{
-<<<<<<< HEAD
      public static void main(String args[]){
-=======
-     public static void main(String args[]){   
->>>>>>> t
         System.out.println("Hello, this is server");
      }
  }
