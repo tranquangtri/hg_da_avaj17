93763757bbd2ba8af73cf9057ac1a2b2558e888f
tranquangtri <tranquangtri96@live.com>
2017-05-14 16:09:50 +0700

    Add interface group, member

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -7,3 +7,11 @@ import java.util.*;
         System.out.println("Hello, this is server");
      }
  }
+ interface Member{
+ 	public int getId();
+ 	public String getName();
+ }
+ interface Group{
+ 	public int getId();
+ 	public List getMembers();
+ }
