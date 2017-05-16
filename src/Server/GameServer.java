41916dd4980942a594fd0016a80e0dc0497741af
tranquangtri <tranquangtri96@live.com>
2017-05-16 12:44:09 +0700

    Mở socket, kết nối client, mỗi accept sẽ tạo 1 thread để xử lý

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -1,17 +1,37 @@
 /**
  * Server entry
  */
+import java.io.*;
+import java.net.*;
 import java.util.*;
  public class GameServer{
-     public static void main(String args[]){
-        System.out.println("Hello, this is server");
+     public static void main(String args[]) throws IOException {
+         System.out.println("#1 Start server");
+         ServerSocket server = null;
+         ArrayList<Socket> clientSockets = new ArrayList<>();
+         /* Open socket */
+         System.out.println("#2 Open socket");
+         server = new ServerSocket(1234);
+         while (true) {
+            /* Accept connection from client */
+             Socket socketAccepted = server.accept();
+             clientSockets.add(socketAccepted);
+             new Thread(new ClientThread(socketAccepted)).start();
+
+             /* Log */
+             System.out.println("Accept " + Integer.toString(clientSockets.size()));
+         }
      }
  }
- interface Member{
- 	public int getId();
- 	public String getName();
- }
- interface Group{
- 	public int getId();
- 	public List getMembers();
+ class ClientThread implements Runnable{
+
+     private Socket clientsocket;
+     public ClientThread(Socket clientsocket){
+         this.clientsocket = clientsocket;
+     }
+     @Override
+     public void run() {
+         // Xử lý đối với client
+
+     }
  }
