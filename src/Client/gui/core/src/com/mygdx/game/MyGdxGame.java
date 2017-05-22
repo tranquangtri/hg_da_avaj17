548fef6d1e39b3fec9e5e4e1fbdbf2abe283241d
tranquangtri <tranquangtri96@live.com>
2017-05-22 23:42:01 +0700

    learn gamestate

--- src/Client/gui/core/src/com/mygdx/game/MyGdxGame.java
+++ src/Client/gui/core/src/com/mygdx/game/MyGdxGame.java
@@ -11,76 +11,30 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
 
-class GameObject{
-	ArrayList<Sprite> arrayList;
-	ArrayList<Float> widthdest;
-	ArrayList<Float> widthdelta;
-	public GameObject(){
-		arrayList = new ArrayList<>();
-		widthdest = new ArrayList<>();
-		widthdelta = new ArrayList<>();
-		for (int i = 2; i <= 14; i++) {
-			Sprite sprite1 = new Sprite(new Texture("cards/" + Integer.toString(i) + ".png"));
-			sprite1.setSize(8 , 8 * sprite1.getHeight() /  sprite1.getWidth());
-			sprite1.setPosition(46,30);
-			widthdest.add( 11.0f + (i - 2) * 6.0f);
-			widthdelta.add(46 - widthdest.get(i-2));
-			arrayList.add(sprite1);
-		}
-
-	}
-	int time = 0;
-	Object mutex = new Object();
-	public void changeMode(){
-		synchronized (mutex){
-			mode =1;
-		}
-	}
-	int mode = 0;
-	public void draw(SpriteBatch batch){
-		int count = 0;
-		for (Sprite item : arrayList) {
-			item.draw(batch);
-			count++;
-		}
-	}
-	float deltatime = 0;
-	public void changed(){
-		int index= 0;
-		float time = Gdx.graphics.getDeltaTime();
-		deltatime += time;
-		if (deltatime > 1) return;
-		for (Sprite item : arrayList) {
-			float velocite = widthdelta.get(index);
-
-			item.setPosition(item.getX()-velocite*(time), item.getY() - (30.0f-10.0f)*(time));
-			index++;
-		}
-	}
-	public void dispose(){
-		for (Sprite item : arrayList){
-			item.getTexture().dispose();
-		}
-	}
-}
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	GameObject gameObject;
-	Camera camera;
-	int a = 0;
-	Sprite bgr;
+	public static final int WIDTH_WORLD = 100;
+	public static final int HEIGHT_WORLD = 100;
+	private GameWorld world;
+	private SpriteBatch batch;
+	private Camera camera;
+	private float width_windows;
+	private float height_windows;
+	private GameWorld gameWorld;
 	@Override
 	public void create () {
+		width_windows = Gdx.graphics.getWidth();
+		height_windows = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
-		gameObject = new GameObject();
-		float width = Gdx.graphics.getWidth();
-		float height = Gdx.graphics.getHeight();
-		camera = new OrthographicCamera(100, 100 * (height /width));
+
+		camera = new OrthographicCamera(WIDTH_WORLD, HEIGHT_WORLD * (height_windows /width_windows));
 		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
-		bgr = new Sprite(new Texture("bgr.jpg"));
-		bgr.setPosition(0,0);
-		bgr.setSize(100,100);
 		camera.update();
+
+		gameWorld = GameWorld.getInstance();
+		IGameScene myGameScene = new MyGameScene();
+		gameWorld.load(myGameScene);
+		gameWorld.setCamera(camera);
 	}
 
 	@Override
@@ -88,16 +42,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
+		gameWorld.handleInput();
+		gameWorld.update();
 		batch.begin();
-		bgr.draw(batch);
-		gameObject.draw(batch);
+		/*--------------------*/
+
+		gameWorld.draw(batch);
+
+		/*--------------------*/
 		batch.end();
-		gameObject.changed();
+
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		gameObject.dispose();
+		gameWorld.getGameScene().dispose();
 	}
 }
