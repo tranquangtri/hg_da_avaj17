3214bf5d6ef580f1b3fd84cec3f4980403da78dc
tranquangtri <tranquangtri96@live.com>
2017-05-27 02:26:06 +0700

    update

---
+++ batfile_test/run4client.bat
@@ -0,0 +1,5 @@
+call cd ..\client\build\libs
+start java -jar client-1.0.jar
+start java -jar client-1.0.jar
+start java -jar client-1.0.jar
+start java -jar client-1.0.jar
---
+++ batfile_test/runserver.bat
@@ -0,0 +1,2 @@
+call cd ..\server\build\libs
+start java -jar server-1.0-all.jar
